5f13773f43c454af19cda730302dcf686002a280
KhatunaKhatuna <kh.chitishvili@gmail.com>
2024-03-17 16:19:48 +0400

    Change color of Header and Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,22 +3,22 @@ export default function Navigation() {
     <nav className="">
       <ul className="flex text-[#fddddd] gap-5 text-xl font-medium">
         <li>
-          <a href="#" className="hover:text-[#45CEC9] transition duration-300">
+          <a href="#" className="hover:text-[#6B72FE] transition duration-300">
             Home
           </a>
         </li>
         <li>
-          <a href="#" className="hover:text-[#45CEC9] transition duration-300">
+          <a href="#" className="hover:text-[#6B72FE] transition duration-300">
             Products
           </a>
         </li>
         <li>
-          <a href="#" className="hover:text-[#45CEC9] transition duration-300">
+          <a href="#" className="hover:text-[#6B72FE] transition duration-300">
             About
           </a>
         </li>
         <li>
-          <a href="#" className="hover:text-[#45CEC9] transition duration-300">
+          <a href="#" className="hover:text-[#6B72FE] transition duration-300">
             Contact
           </a>
         </li>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../../logo.svg";
 import Navigation from "../Navigation";
 export default function Header() {
   return (
-    <header className="bg-darck-blue/95 sticky top-0 z-10">
+    <header className="bg-[rgb(25,25,25)]/95 sticky top-0 z-10">
       <div className="flex items-center justify-between max-w-[90%] my-0 mx-auto py-[1rem]">
         <a href="/">
           <img className="h-12" src={logo} alt="logo" />
