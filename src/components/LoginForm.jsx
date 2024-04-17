b29b913a7229602df26352bb38afee5cff2630a1
KhatunaKhatuna <kh.chitishvili@gmail.com>
2024-04-18 00:18:09 +0400

    Add authentication, add arror

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -16,19 +16,24 @@ export default function LoginForm({ handleLogin }) {
     }));
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!username || !password) return;
+    handleLogin(username, password);
+    setLoginData({
+      username: "",
+      password: "",
+    });
+  }
+
   return (
-    <section className="text-[#fdf2e9]  flex-1 flex  justify-center items-center ">
+    <section className="text-[#fdf2e9] flex  justify-center items-center mt-[200px]">
       <div className="flex gap-5 flex-col justify-center items-center max-w-[80%] my-0 mx-auto">
         <div className="bg-[#2D2E35] px-[2rem] py-[1rem] rounded-3xl">
           <h2 className="text-[2rem] leading-[25px] text-center pb-[1rem] ">
             Login
           </h2>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-            }}
-            className="grid grid-cols-1 gap-4"
-          >
+          <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-4">
             <input
               className="w-full p-1.5 text-[#0e0e0e]/95 rounded-[5px]  focus:border-[#6B72FE] focus:outline-[#6B72FE]"
               maxLength="100"
@@ -57,10 +62,7 @@ export default function LoginForm({ handleLogin }) {
             />
 
             <div className="flex gap-3">
-              <button
-                onClick={() => handleLogin(username, password)}
-                className=" w-full  border-0 bg-[#6B72FE] py-1.5 mx-auto text-[#fdf2e9] lg:text-[20px] rounded-[5px] hover:bg-[#333B61] transition-all transform duration-300 ease-linear"
-              >
+              <button className=" w-full  border-0 bg-[#6B72FE] py-1.5 mx-auto text-[#fdf2e9] lg:text-[20px] rounded-[5px] hover:bg-[#333B61] transition-all transform duration-300 ease-linear">
                 Login
               </button>
             </div>
