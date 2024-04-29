afe8090f5e97013a1bebcbdc821e64bc5ecf890a
KhatunaKhatuna <kh.chitishvili@gmail.com>
2024-04-29 20:53:53 +0400

    Add hover and active to LangSwitcher and darkMode components

--- src/components/LangSwitcher.jsx
+++ src/components/LangSwitcher.jsx
@@ -1,33 +1,47 @@
-'use client'
+"use client";
 
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 import { useLocale } from "next-intl";
-import Link from 'next/link';
+import Link from "next/link";
 const LangSwitcher = () => {
+  const locale = useLocale();
+  let pathname = usePathname();
+  let langEn = "";
+  let langKa = "";
 
-    const locale = useLocale();
-    let pathname = usePathname();
-    let langEn = '';
-    let langKa = '';
-
-    if (pathname === '/en') {
-        langKa = '/ka';
-    } else if (pathname === '/ka') {
-        langEn = '/en';
-    } else {
-        if (locale === 'en') {
-            langKa = pathname.replace('/en/', '/ka/');
-        } else if (locale === 'ka') {
-            langEn = pathname.replace('/ka/', '/en/');
-        }
+  if (pathname === "/en") {
+    langKa = "/ka";
+  } else if (pathname === "/ka") {
+    langEn = "/en";
+  } else {
+    if (locale === "en") {
+      langKa = pathname.replace("/en/", "/ka/");
+    } else if (locale === "ka") {
+      langEn = pathname.replace("/ka/", "/en/");
     }
+  }
 
-    return (
-        <div className="cursor-pointer">
-            <Link href={`${langEn}`} className={`p-3 text-[18px] hover:text-orange transition-all transform duration-300 ease-linear ${locale === 'en' ? 'text-orange' : ''}`}>en</Link> |
-            <Link href={`${langKa}`} className={`p-3 text-[18px] hover:text-orange transition-all transform duration-300 ease-linear ${locale === 'ka' ? 'text-orange' : ''}`}>ka</Link>
-        </div>
-    );
-}
+  return (
+    <div className="cursor-pointer">
+      <Link
+        href={`${langEn}`}
+        className={`p-3 text-[20px] hover:text-[#6B72FE] transition-all transform duration-300 ease-linear ${
+          locale === "en" ? "text-[#6B72FE]" : ""
+        }`}
+      >
+        en
+      </Link>{" "}
+      |
+      <Link
+        href={`${langKa}`}
+        className={`p-3 text-[20px] hover:text-[#6B72FE] transition-all transform duration-300 ease-linear ${
+          locale === "ka" ? "text-[#6B72FE]" : ""
+        }`}
+      >
+        ka
+      </Link>
+    </div>
+  );
+};
 
 export default LangSwitcher;
