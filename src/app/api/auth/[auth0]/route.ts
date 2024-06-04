d93dfdb3d791c936f8ad8b63c8be54d552fde6a9
KhatunaKhatuna <kh.chitishvili@gmail.com>
2024-06-04 14:26:22 +0400

    create users and add to db afret auth

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,10 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  login: handleLogin({
+    returnTo: "/api/create-user",
+    authorizationParams: {
+      prompt: "login",
+    },
+  }),
+});
--- src/app/api/create-user/route.ts
+++ src/app/api/create-user/route.ts
@@ -1,17 +1,41 @@
 import { sql } from "@vercel/postgres";
-import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { getSession } from "@auth0/nextjs-auth0";
+import { redirect } from "next/navigation";
 
-export async function POST(request: Request) {
-  const { name, email, age } = await request.json();
-
+export async function GET(_: NextRequest) {
   try {
-    if (!name || !email || !age)
-      throw new Error("name, email and age are required");
-    await sql`INSERT INTO users (name, email, age) VALUES (${name}, ${email}, ${age});`;
+    const session = await getSession();
+
+    if (session?.user) {
+      const { name, email, sub, picture } = session.user;
+
+      const user = await sql`SELECT * FROM users WHERE sub = ${sub}`;
+
+      if (!user.rows.length)
+        await sql`INSERT INTO users (name, email, sub, image_url) VALUES (${name}, ${email}, ${sub}, ${picture});`;
+    }
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    return redirect("/api/auth/logout");
   }
 
-  const users = await sql`SELECT * FROM users;`;
-  return NextResponse.json({ users }, { status: 200 });
+  return redirect("/");
 }
+
+// import { sql } from "@vercel/postgres";
+// import { NextResponse } from "next/server";
+
+// export async function POST(request: Request) {
+//   const { name, email, age } = await request.json();
+
+//   try {
+//     if (!name || !email || !age)
+//       throw new Error("name, email and age are required");
+//     await sql`INSERT INTO users (name, email, age) VALUES (${name}, ${email}, ${age});`;
+//   } catch (error) {
+//     return NextResponse.json({ error }, { status: 500 });
+//   }
+
+//   const users = await sql`SELECT * FROM users;`;
+//   return NextResponse.json({ users }, { status: 200 });
+// }
